d2f12b784809a976b214e45b99f5d461c0b9d235
Andreas Limberopoulos <limbera@gmail.com>
2016-02-07 17:22:44 +1100

    Form ajax submit to google sheets

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,61 @@ $('#whatHeadLink').on('click', function(event) {
         }, 1000);
     }
 });
+
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#demoForm").submit(function(event){
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // fire off the request to Google Sheets
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbz6z_GUHtRsFViKePupclc9BNAV5y6qI6buBXVR59wxvJxnd_Y/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
